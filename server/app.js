f9c13d8b76d7d333896eb7651d149e9a40757dc2
Hershman <212458881@logon.ds.ge.com>
2017-07-14 09:21:45 -0400

    changed title

--- server/app.js
+++ server/app.js
@@ -242,7 +242,7 @@ app.use(function(err, req, res, next) {
 	}
 });
 
-httpServer.listen(process.env.VCAP_APP_PORT || 6001, function () {
+httpServer.listen(process.env.VCAP_APP_PORT || 6005, function () {
 	console.log ('Server started on port: ' + httpServer.address().port);
 });
 
